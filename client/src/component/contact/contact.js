c99b468934ce67a0baac50cb9870e88d23117467
Henri-Thibault Huyghe <henri-thibault.huyghe@student.howest.be>
2018-07-04 17:14:12 +0200

    Mailto opens new tab fixed

--- client/src/component/contact/contact.js
+++ client/src/component/contact/contact.js
@@ -9,7 +9,7 @@ class Contact extends Component {
           <p>Want to get into contact with me? Great! Feel free to use any of the services below.</p>
           <div>
             <a id="linkedin" href="https://www.linkedin.com/in/henri-thibault-huyghe-aa82a2159/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-            <a id="mail" href="mailto:henrithibault.huyghe@gmail.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-envelope"></i></a>
+            <a id="mail" href="mailto:henrithibault.huyghe@gmail.com"><i class="fas fa-envelope"></i></a>
             <a id="github" href="https://github.com/HuygheHenri-Thibault" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
           </div>
           <p className="end-tag">&lt;/HUYGHE&gt;</p>

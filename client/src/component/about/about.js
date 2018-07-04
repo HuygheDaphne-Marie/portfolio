052d1d264488a92b7956500a31039d129b639e24
Henri-Thibault Huyghe <henri-thibault.huyghe@student.howest.be>
2018-07-04 16:02:44 +0200

    More gramitical fixes thanks to Fredrik

--- client/src/component/about/about.js
+++ client/src/component/about/about.js
@@ -11,7 +11,7 @@ class About extends Component {
             <img src={require('../images/portrait.png')} alt="Portrait" />
             <div>
                 <h2>Hi there, my name is Henri&#8209;Thibault.</h2>
-                <p>Welcome to my site. I'm a {age} year old applied computer science student at Howest College University. I am currently in my 2nd year of a 3 year bachelors degree specializing in computer & cyber security. My hobbies are gaming, writing and roleplay games. Next semester I'll be studying abroad in Austria with Erasmus which I'll be writing a blog about.</p>
+                <p>Welcome to my site. I'm a {age} year old applied computer science student at Howest College University. I am currently in my 2nd year of a 3 year bachelors degree specializing in computer & cyber security. My hobbies are gaming, writing and roleplaying games. Next semester I'll be studying abroad in Austria with Erasmus which I'll be writing a blog about.</p>
             </div>
         </article>
       </section>
